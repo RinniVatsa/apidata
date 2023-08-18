b7e9e84f1d01f0d88732a0b630d41613746fe358
Rinni Vatsa <rinnivatsa09@gmail.com>
2023-08-18 22:41:12 +0530

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,9 @@ import FirstPage from './pages/FirstPage';
 import SecondPageComponent1 from './pages/SecondPageComponent1';
 import SecondPageComponent2 from './pages/SecondPageComponent2';
 
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-// Create a light theme with updated colors
-const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: {
-      main: '#1976d2', // Update with your desired light primary color
-    },
-    secondary: {
-      main: '#f50057', // Update with your desired light secondary color
-    },
-    // Customize other palette colors as needed
-  },
-  // Add other theme customization options here
-});
+
+
 
 function App() {
   
@@ -29,7 +16,7 @@ function App() {
 
 
     <Router>
-          <ThemeProvider theme={lightTheme}></ThemeProvider>
+        
       <Routes>
         <Route  path="/" element={ <FirstPage/>} />
         <Route path="/second"   element={<SecondPageComponent1/>} />
